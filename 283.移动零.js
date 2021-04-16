0a2b37ee84b17fd64bec32995a678cacc9a64586
luoxifeng <942394682@qq.com>
2021-04-16 12:02:04 +0800

    Update 283.移动零.js

--- 283.移动零.js
+++ 283.移动零.js
@@ -34,15 +34,12 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
-  for (var i = nums.length - 1; i >= 0;) {
-    if (nums[i] === 0) {
-      nums.splice(i, 1)
-      nums.push(0)
-    }
+var moveZeroes = function (nums, ) {
+  let i = nums.length - 1
+  while (i >= 0) {
+    if (nums[i] === 0) nums.push(...nums.splice(i, 1))
     i--
   }
-
 };
 // @lc code=end
 
